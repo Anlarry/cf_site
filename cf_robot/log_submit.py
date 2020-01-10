fa687fc9345a665af405124028fac85e53627435
Anlaryy <2944419175@qq.com>
2020-01-10 06:26:04 +0000

    from pythonanywhere

--- cf_robot/log_submit.py
+++ cf_robot/log_submit.py
@@ -122,8 +122,8 @@ class Log_Submit:
         res = self.s.get(url)
         print(res)
         html = res.text
-        with open('cf_robot/tmp.html', 'w') as F:
-            print(html, file=F)
+        #with open('cf_robot/tmp.html', 'w') as F:
+        #    print(html, file=F)
         bs = BeautifulSoup(html, "lxml")
         self.submit_id = bs.find(attrs={'class':'hiddenSource'}).get_text()
         verdict = bs.find(attrs={'submissionid':self.submit_id}).get_text()
